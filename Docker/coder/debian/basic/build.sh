2ac25336d9986608ae04d3aa0e00530376c3fb87
G r e y <grey@coder.com>
2021-10-23 20:37:51 +0000

    feat: rewrite vapurrmaid/coder
    Summary:
    
    Improved vapurrmaid/coder for Go, NodeJS (npm + yarn) support.
    
    Details:
    
    - remove configure script (including NVM)
    - update ubuntu to focal
    - remove ppa:peek-developers
    - remove emacs (unused at this point)

--- Docker/coder/debian/basic/build.sh
+++ Docker/coder/debian/basic/build.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# build.sh
+#
+# Dependencies: docker
+# 
+# Description: Builds the vapurrmaid/coder image defined in Dockerfile
 
 set -euox pipefail
 
-docker build -t vapurrmaid/coder:basic .
-docker push vapurrmaid/coder:basic
+docker build -t vapurrmaid/coder:latest .
+docker push vapurrmaid/coder:latest
--- Docker/coder/debian/emacs/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euox pipefail
-
-docker build -t vapurrmaid/coder:emacs .
-docker push vapurrmaid/coder:emacs
